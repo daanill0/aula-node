79f573e06e445f8c95346413ff5eaa5b5b1e8a9b
Aluno <cdanilopio@gmail.com>
2025-03-11 20:21:21 -0300

    inicio

---
+++ aula1.js
@@ -0,0 +1,19 @@
+var nome = "dan";
+var idade = 11;
+var altura = 1.80;
+var andando = false;
+
+if (idade>40)
+{
+    console.log("idoso")
+}
+else if (idade >= 18)
+{
+    console.log("adulto");
+}else if (idade>=12)
+{
+    console.log("adolecente");
+}else 
+{
+    console.log("criança")
+}
---
+++ aula2.js
@@ -0,0 +1,18 @@
+let aluno = "pedro";
+
+let alunos = ["pedro","ana","joao",123];
+
+
+
+let ini = 0 ;
+let fim = alunos.length - 1;
+alunos.sort();
+
+
+while(ini <= fim)
+{
+    console.log(alunos[ini]);
+    
+    ini = ini + 1
+
+}
